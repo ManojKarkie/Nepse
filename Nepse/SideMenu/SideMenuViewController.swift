053422dd9229330dca1da3d96c9da6604224a860
lizan <lizan@ekbana.net>
2017-09-05 23:24:03 +0545

    added side menu and dashboard

---
+++ Nepse/SideMenu/SideMenuViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SideMenuViewController.swift
+//  Nepse
+//
+//  Created by Lizan Pradhanang on 9/5/17.
+//  Copyright © 2017 Swiftech. All rights reserved.
+//
+
+import UIKit
+
+class SideMenuViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
