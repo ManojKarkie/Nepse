f46b076eaf3d7162d36ec946b6ac019d8805b233
lizan <lizan@ekbana.net>
2017-09-06 10:51:32 +0545

    Dashboard design implemented

--- Nepse/Dashboard/DashboardViewController.swift
+++ Nepse/Dashboard/DashboardViewController.swift
@@ -10,26 +10,53 @@ import UIKit
 
 class DashboardViewController: UIViewController {
 
+    @IBOutlet weak var chartView: UIView!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var chartViewContainer: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setup()
+        tableView.delegate = self
+        tableView.dataSource = self
         // Do any additional setup after loading the view.
+    }
+    
+    func setup() {
+        chartViewContainer.layer.cornerRadius = chartViewContainer.frame.size.height/8
+        chartViewContainer.layer.borderColor = UIColor.lightGray.cgColor
+        chartViewContainer.layer.borderWidth = 1
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+       
     }
     
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+extension DashboardViewController: UITableViewDelegate {
+    
+}
+
+extension DashboardViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+}
 
+class RoundCornerButton: UIButton {
+    
+    open override func awakeFromNib() {
+        self.layer.cornerRadius = self.frame.size.height/8
+        self.layer.borderWidth = 1
+        self.layer.borderColor = UIColor.lightGray.cgColor
+    }
 }
